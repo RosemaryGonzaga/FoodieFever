1e722a04feac7b6c488925bd4a28c2bdf1ac237d
RosemaryGonzaga <rosemary.gonzaga@gmail.com>
2019-06-13 16:32:53 -0400

    Add time slider, but need to edit it to re-render the map instead of adding a new one

---
+++ src/geomap.js
@@ -0,0 +1,141 @@
+export const renderGeoMap = (year) => { // later, this should take in a dataset too
+    const width = 720;
+    const height = 500;
+
+    const projection = d3.geoAlbersUsa()
+        .scale(1000)
+        .translate([width / 2, height / 2]);
+
+    const path = d3.geoPath()
+        .projection(projection);
+
+    let svg = d3.select(".geomap-container").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    let color = d3.scaleThreshold()
+        .domain([0, 0.15, 0.30, 0.45, 0.55, 0.70, 0.85, 1.0])
+        .range(["lightgray", "#009392", "#39B185", "#9CCB86", "#E9E29C", "#EEB479", "#E88471", "#CF597E"]); // color scheme 1 (green-red)
+    // .range(["lightgray", "#228B3B", "#6CBA7D", "#CDE5D2", "#FCE1A4", "#FABF7B", "#E05C5C", "#AB1866"]); // color scheme 2 (green-magenta)
+    // .range(["lightgray", "#3C93C2", "#6CB0D6", "#9EC9E2", "#E1F2E3", "#FEB24C", "#FD8D3C", "#FC4E2A"]); // color scheme 3 (blue-orange)
+
+    d3.json("assets/data/cb_2018_us_state_5m.json").then(us => {
+
+        // Note: I should prob refactor the below line later to interpolate the food comparison into the filepath
+        d3.csv("assets/data/sriracha/sriracha_vs_tabasco_geo_trended.csv").then(data => {
+
+            const filteredData = data.filter(datum => datum.year === year); // refactored to match a variable year, which should come in as a string
+            let srirachaSearchFreqByState = {};
+            filteredData.forEach(datum => {
+                if (datum.sriracha === "0" && datum.tabasco === "0") {
+                    srirachaSearchFreqByState[datum.Region] = -0.2;
+                } else {
+                    srirachaSearchFreqByState[datum.Region] = parseFloat(datum.sriracha);
+                }
+            });
+
+            svg.append("g")
+                .attr("class", "states")
+                .selectAll("path")
+                .data(topojson.feature(us, us.objects.cb_2018_us_state_5m).features)
+                .enter()
+                .append("path")
+                .attr("d", path)
+                .style("fill", d => {
+                    let searchFreq = srirachaSearchFreqByState[d.properties.NAME];
+                    return color(searchFreq);
+                });
+
+        });
+    });
+}
+
+
+
+
+
+    // TUTORIAL CODE
+    // Global variables to store slider state and "dictionary" (or reference values for input)
+    // var inputValue = null;
+    // var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    // // Dimensions of visualization (svg)
+    // let width = 700;
+    // let height = 580;
+
+    // // Create SVG canvas (add svg to DOM)
+    // let svg = d3.select('body')
+    //     .append('svg')
+    //     .attr('width', width)
+    //     .attr('height', height);
+
+    // // MAP *****************************************************************
+
+    // // RODENT DATA *****************************************************************
+    // let rodents = svg.append('g')
+    // rodents.selectAll('path')
+    //     .data(rodents_json.features)    // how does d3's .features method know to extract the coordinates?
+    //     .enter()
+    //     .append('path')
+    //     .attr('fill', initialDate)
+    //     .attr('stroke', '#999')
+    //     .attr('d', geoPath)    // position the dots based on the geoPath function?
+    //     .attr('class', 'incident') // add class to each data point (dot) so it can be styled with css (defined in head)
+    //     // .on('mouseover', d => {      // NOTE: using a fat arrow function yields this error: this.setAttribute is not a function (b/c it binds this?)
+    //     //     d3.select('h2').text(d.properties.LOCATION_STREET_NAME);
+    //     //     d3.select(this).attr('class', 'incident hover');
+    //     // })
+    //     // .on('mouseout', d => {
+    //     //     d3.select('h2').text('');
+    //     //     d3.select(this).attr('class', 'incident');
+    //     // });
+    //     // EVENT LISTENERS FOR INTERACTIVITY (HOVER)
+    //     .on('mouseover', function (d) {
+    //         d3.select('h2').text(d.properties.LOCATION_STREET_NAME);
+    //         d3.select(this).attr('class', 'incident hover');
+    //     })
+    //     .on('mouseout', function (d) {
+    //         d3.select('h2').text('');
+    //         d3.select(this).attr('class', 'incident');
+    //     });
+
+
+    // // EVENT LISTNERS FOR INTERACTIVITY (TIME SLIDER) ******************************************
+    // // Each event listener takes in a callback that's subsequently defined below
+
+    // // When input range changes, update value
+    // d3.select('#timeslide').on('input', function () {     // TEST: will a big arrow function break this? YES (value is undefined)
+    //     update(+this.value);    // what is the '+' for??? code seems to function well without it...
+    // });
+
+    // // Update the fill of each SVG element of class "incident" with value
+    // function update(value) {    // TEST: will a big arrow function break this? maybe b/c of hoisting and/or context binding?
+    //     document.getElementById('range').innerHTML = month[value];   // set the month by indexing into month array at an index of the range's value
+    //     inputValue = month[value];  // update global var
+    //     d3.selectAll('.incident')
+    //         .attr('fill', dateMatch);   // yet to define dateMatch, a fct that will check the inputValue for a match w/ data and return a color if there's a match
+    // }
+
+    // // Function to return a color for data point (based on color match status)
+    // function dateMatch(data, value) {   // why do we need to take in a value parameter if it never gets used? Code works ok without it
+    //     let d = new Date(data.properties.OPEN_DT);  // create a JS Date object using the data
+    //     let m = month[d.getMonth()];    // get month using Date method
+    //     if (inputValue === m) {     // check for match against input value
+    //         this.parentElement.appendChild(this);   // help with layering??? (so it draws last and is on top?)
+    //         return 'red';
+    //     } else {
+    //         return '#999';
+    //     };
+    // }
+
+    // // Set the initial state (when map loads, initial state is set to January)
+    // function initialDate(data, i) {
+    //     let d = new Date(data.properties.OPEN_DT);
+    //     let m = month[d.getMonth()];
+    //     if (m === "January") {
+    //         this.parentElement.appendChild(this);
+    //         return 'red';
+    //     } else {
+    //         return '#999';
+    //     };
+    // }
--- src/index.js
+++ src/index.js
@@ -1,121 +1,21 @@
+import { renderGeoMap } from './geomap';
+
 document.addEventListener("DOMContentLoaded", () => {
-    // alert('hello!!!');
-    // console.log('this should print in the console!')
-    // debugger
 
-    const width = 720,
-        height = 500;
+    // global vars for storing input values of slider
+    var inputValue = null;
+    var year = [
+        "2006", "2007", "2008", 
+        "2009", "2010", "2011", 
+        "2012", "2013", "2014", 
+        "2015", "2016", "2017", 
+        "2018", "2019"
+    ];
 
-    const projection = d3.geoAlbersUsa()
-        .scale(1000)
-        .translate([width / 2, height / 2]);
-
-    const path = d3.geoPath()
-        .projection(projection);
-
-    let svg = d3.select("body").append("svg")
-        .attr("width", width)
-        .attr("height", height);
-
-    let color = d3.scaleThreshold()
-        .domain([0, 0.15, 0.30, 0.45, 0.55, 0.70, 0.85, 1.0])
-        .range(["lightgray", "#009392", "#39B185", "#9CCB86", "#E9E29C", "#EEB479", "#E88471", "#CF597E"]); // color scheme 1 (green-red)
-        // .range(["lightgray", "#228B3B", "#6CBA7D", "#CDE5D2", "#FCE1A4", "#FABF7B", "#E05C5C", "#AB1866"]); // color scheme 2 (green-magenta)
-        // .range(["lightgray", "#3C93C2", "#6CB0D6", "#9EC9E2", "#E1F2E3", "#FEB24C", "#FD8D3C", "#FC4E2A"]); // color scheme 3 (blue-orange)
-
-    d3.json("assets/data/cb_2018_us_state_5m.json").then(us => {
-
-        // Note: I should prob refactor the below line later to interpolate the food comparison into the filepath
-        // d3.csv("assets/data/sriracha_tabasco_timeline_2004_to_present.csv").then(hotsauceTimelineData => {
-        // d3.csv("assets/data/sriracha/sriracha_tabasco_geomap_2006.csv").then(hotsauceGeoData2006 => {
-        d3.csv("assets/data/sriracha/sriracha_vs_tabasco_geo_trended.csv").then(data => {
-            // console.log(us);
-            // console.log(us.objects.cb_2018_us_state_5m);
-            // console.log(topojson.feature(us, us.objects.cb_2018_us_state_5m));
-            // console.log(data);
-            // console.log(data[0]);
-            // console.log(data[data.length - 1]);
-            // console.log(getSrirachaSearchFreqByState(data, "2013"));
-            // console.log(data);
-            // console.log(filteredData);
-            // console.log(srirachaSearchFreqByState);
-
-
-            // const filteredData = data.filter(datum => datum.year === "2006"); // the year is hardcoded right now --> need to use a year variable that will get passed in from outside
-            // const filteredData = data.filter(datum => datum.year === "2007"); // the year is hardcoded right now --> need to use a year variable that will get passed in from outside
-            // const filteredData = data.filter(datum => datum.year === "2008"); // the year is hardcoded right now --> need to use a year variable that will get passed in from outside
-            // const filteredData = data.filter(datum => datum.year === "2009"); // the year is hardcoded right now --> need to use a year variable that will get passed in from outside
-            // const filteredData = data.filter(datum => datum.year === "2010"); // the year is hardcoded right now --> need to use a year variable that will get passed in from outside
-            // const filteredData = data.filter(datum => datum.year === "2011"); // the year is hardcoded right now --> need to use a year variable that will get passed in from outside
-            // const filteredData = data.filter(datum => datum.year === "2012"); // the year is hardcoded right now --> need to use a year variable that will get passed in from outside
-            // const filteredData = data.filter(datum => datum.year === "2013"); // the year is hardcoded right now --> need to use a year variable that will get passed in from outside
-            // const filteredData = data.filter(datum => datum.year === "2014"); // the year is hardcoded right now --> need to use a year variable that will get passed in from outside
-            // const filteredData = data.filter(datum => datum.year === "2015"); // the year is hardcoded right now --> need to use a year variable that will get passed in from outside
-            // const filteredData = data.filter(datum => datum.year === "2016"); // the year is hardcoded right now --> need to use a year variable that will get passed in from outside
-            // const filteredData = data.filter(datum => datum.year === "2017"); // the year is hardcoded right now --> need to use a year variable that will get passed in from outside
-            // const filteredData = data.filter(datum => datum.year === "2018"); // the year is hardcoded right now --> need to use a year variable that will get passed in from outside
-            const filteredData = data.filter(datum => datum.year === "2019"); // the year is hardcoded right now --> need to use a year variable that will get passed in from outside
-            let srirachaSearchFreqByState = {};
-            filteredData.forEach(datum => {
-                if (datum.sriracha === "0" && datum.tabasco === "0") {
-                    srirachaSearchFreqByState[datum.Region] = -0.2; // or should it be 0.5?
-                } else {
-                    srirachaSearchFreqByState[datum.Region] = parseFloat(datum.sriracha);
-                }
-            });
-
-            svg.append("g")
-                .attr("class", "states")
-                .selectAll("path")
-                .data(topojson.feature(us, us.objects.cb_2018_us_state_5m).features)
-                .enter()
-                .append("path")
-                .attr("d", path)
-                // .style("fill", "#1a6b1a");  // ultimately, we want to fill the color dynamically
-                .style("fill", d => {
-                    let searchFreq = srirachaSearchFreqByState[d.properties.NAME];
-                    // console.log(d);
-                    // console.log(d.properties.NAME);
-                    return color(searchFreq);
-                    // return "purple";
-                });
-
-        });
+    d3.select("#timeslide").on("input", function() {
+        document.getElementById("range").innerHTML = year[this.value];
+        // d3.select("svg").remove();  // this takes too long to re-render
+        renderGeoMap(`${year[this.value]}`);
     });
-
-
-    // IMPORTANT NOTE: RIGHT NOW, THIS IS CODE I WROTE FOR A TUTORIAL, AS PROOF OF CONCEPT
-    // NOT MY OWN PROJECT CODE (but I didn't just copy / paste)
-    // Tutorial source: http://duspviz.mit.edu/d3-workshop/mapping-data-with-d3/
-
-    // TUTORIAL CODE
-    // d3.json("assets/data/us.json").then(us => {
-    //     d3.tsv("assets/data/us_unemployment_2008.tsv").then(unemployment => {   // load real Google trends data here
-    //         console.log(us.objects.states);
-    //         console.log(unemployment);
-
-    //         let rateById = {};  // empty object to hold dataset
-    //         unemployment.forEach(d => {
-    //             rateById[d.id] = d.rate;    // populate object with each county's rate (retrieve and set at a key of each county's id)
-    //         });
-    //         // console.log(rateById);
-
-    //         svg.append("g")
-    //             .attr("class", "states")
-    //             .selectAll("path")
-    //             .data(topojson.feature(us, us.objects.states).features) // Bind TopoJSON data elements
-    //             .enter().append("path")
-    //             .attr("d", path)
-    //             // .style("fill", d => {
-    //             //     let rateValue = rateById[d.id]; // get unemployment rate for the given data point (match by id)
-    //             //     return color(rateValue);    // pass unemployment rate into color function (defined below) to get the correct fill color
-    //             // });
-    //             .style("fill", "#008080");
-    //     });
-
-    //     let color = d3.scaleThreshold()
-    //         .domain([0.02, 0.04, 0.06, 0.08, 0.10])
-    //         .range(["#F0F0F0", "#b2d8d8", "#66b2b2", "#008080", "#006666", "#004c4c"]);
-    //         // .range(["#f2f0f7", "#dadaeb", "#bcbddc", "#9e9ac8", "#756bb1", "#54278f"]);  // different color palette
-    // });
+    renderGeoMap(`2006`);
 });

2cacc93e7e7f658bb8be8d5e618c001661bf33d8
RosemaryGonzaga <rosemary.gonzaga@gmail.com>
2019-06-13 14:10:35 -0400

    Functional color map with pilot data (sriracha vs tabasco, 2006)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // IMPORTANT NOTE: RIGHT NOW, THIS IS CODE I WROTE FOR A TUTORIAL, AS PROOF OF CONCEPT
-    // NOT MY OWN PROJECT CODE (but I didn't just copy / paste)
-    // Tutorial source: http://duspviz.mit.edu/d3-workshop/mapping-data-with-d3/
-
     // alert('hello!!!');
     // console.log('this should print in the console!')
     // debugger
@@ -21,18 +17,38 @@ document.addEventListener("DOMContentLoaded", () => {
         .attr("width", width)
         .attr("height", height);
 
+    let color = d3.scaleThreshold()
+        .domain([0, 0.15, 0.30, 0.45, 0.55, 0.70, 0.85, 1.0])
+        .range(["lightgray", "#009392", "#39B185", "#9CCB86", "#E9E29C", "#EEB479", "#E88471", "#CF597E"]);
+        // .domain([0.02, 0.04, 0.06, 0.08, 0.10])
+        // .range(["#F0F0F0", "#b2d8d8", "#66b2b2", "#008080", "#006666", "#004c4c"]);
+
     d3.json("assets/data/cb_2018_us_state_5m.json").then(us => {
 
         // Note: I should prob refactor the below line later to interpolate the food comparison into the filepath
         // d3.csv("assets/data/sriracha_tabasco_timeline_2004_to_present.csv").then(hotsauceTimelineData => {
         // d3.csv("assets/data/sriracha/sriracha_tabasco_geomap_2006.csv").then(hotsauceGeoData2006 => {
-        d3.csv("assets/data/sriracha/sriracha_vs_tabasco_geo_trended.csv").then(srirachaGeoData => {
+        d3.csv("assets/data/sriracha/sriracha_vs_tabasco_geo_trended.csv").then(data => {
             // console.log(us);
             // console.log(us.objects.cb_2018_us_state_5m);
             // console.log(topojson.feature(us, us.objects.cb_2018_us_state_5m));
-            console.log(srirachaGeoData);
-            console.log(srirachaGeoData[0]);
-            console.log(srirachaGeoData[srirachaGeoData.length - 1]);
+            // console.log(data);
+            // console.log(data[0]);
+            // console.log(data[data.length - 1]);
+            // console.log(getSrirachaSearchFreqByState(data, "2013"));
+            // console.log(data);
+            // console.log(filteredData);
+            // console.log(srirachaSearchFreqByState);
+
+            const filteredData = data.filter(datum => datum.year === "2006"); // the year is hardcoded right now --> need to use a year variable that will get passed in from outside
+            let srirachaSearchFreqByState = {};
+            filteredData.forEach(datum => {
+                if (datum.sriracha === "0" && datum.tabasco === "0") {
+                    srirachaSearchFreqByState[datum.Region] = -0.2; // or should it be 0.5?
+                } else {
+                    srirachaSearchFreqByState[datum.Region] = parseFloat(datum.sriracha);
+                }
+            });
 
             svg.append("g")
                 .attr("class", "states")
@@ -41,14 +57,22 @@ document.addEventListener("DOMContentLoaded", () => {
                 .enter()
                 .append("path")
                 .attr("d", path)
-                .style("fill", "#1a6b1a");  // ultimately, we want to fill the color dynamically
+                // .style("fill", "#1a6b1a");  // ultimately, we want to fill the color dynamically
+                .style("fill", d => {
+                    let searchFreq = srirachaSearchFreqByState[d.properties.NAME];
+                    // console.log(d);
+                    // console.log(d.properties.NAME);
+                    return color(searchFreq);
+                    // return "purple";
+                });
 
-            function colorThresholds(year) {
-                
-            }
         });
     });
 
+
+    // IMPORTANT NOTE: RIGHT NOW, THIS IS CODE I WROTE FOR A TUTORIAL, AS PROOF OF CONCEPT
+    // NOT MY OWN PROJECT CODE (but I didn't just copy / paste)
+    // Tutorial source: http://duspviz.mit.edu/d3-workshop/mapping-data-with-d3/
 
     // TUTORIAL CODE
     // d3.json("assets/data/us.json").then(us => {

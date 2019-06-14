88e3591e5a825e2e3b07f89fc926dc03fe086d38
RosemaryGonzaga <rosemary.gonzaga@gmail.com>
2019-06-14 17:13:28 -0400

    Fix overlay of two datasets in scatterplot

--- src/line_chart.js
+++ src/line_chart.js
@@ -9,12 +9,8 @@ export const renderLineChart = dataset => {
         .attr("height", height);
     
     d3.csv(dataset).then(data => {
+
         // Define x & y scales based on dataset
-        // let maxX = data.length;  // The old way: set x value by datum's in the dataset array
-        // let xScale = d3.scaleLinear()
-        //     .domain([0, maxX])
-        //     .range([padding, width - padding]);
-        
         let xScale = d3.scaleTime()
             .domain([new Date(2004, 0, 1), new Date(2019, 0, 6)])
             .range([padding, width - padding]);
@@ -38,7 +34,12 @@ export const renderLineChart = dataset => {
             .attr("transform", "translate(" + padding + ", 0)")
             .call(yAxis);
 
-        // Render data as circles on the chart
+        // Tag and combine tabasco & sriracha data for scatterplot overlay
+        // Note: It didn't work when I tried to append first one column's data (tabasco),
+        // ...then another (sriracha) in two separate data joins and enter / append statements.
+        // ...Not quite sure why it didn't work...might have to do with the datasets being the
+        // ...same size and therefore not registering an enter selection.
+        // ...This implementation is a workaround that selects, joins, and appends both datasets at the same time.
         let tabascoData = data.map(datum => {
             let newRow = Object.assign({}, { Month: datum.Month, tabasco: datum.tabasco });
             return newRow;
@@ -47,64 +48,30 @@ export const renderLineChart = dataset => {
             let newRow = Object.assign({}, { Month: datum.Month, sriracha: datum.sriracha });
             return newRow;
         });
-        // console.log(data);  // this has an extra key with columns; may need to add this to tabascoData & srirachaData
-        // console.log(tabascoData);
-        // console.log(srirachaData);
-        // console.log(typeof data);
-        // console.log(typeof tabascoData);
-        // console.log(typeof srirachaData);
-        
+        let taggedCombinedData = tabascoData.concat(srirachaData);
+
+        // Render data as circles on the chart
         svg.selectAll('circle')
-            // .data(data)
-            .data(tabascoData)
+            .data(taggedCombinedData)
             .enter()
             .append("circle")
-            // .attr("cx", (_, idx) => xScale(idx)) // The old way: set x value by datum's in the dataset array
             .attr("cx", (datum) => {
                 return xScale(new Date(datum.Month))
             })
-            .attr("cy", datum => yScale(datum.tabasco))
-            .attr("r", "2")
-            .attr("fill", "green");
-        svg.selectAll('circle')
-            // .data(data)
-            .data(srirachaData)
-            .exit().remove()
-            .enter()
-            .join("circle")
-            .append("circle")
-            // .attr("cx", (_, idx) => xScale(idx))
-            .attr("cx", (datum) => {
-                return xScale(new Date(datum.Month))
+            .attr("cy", datum => {
+                if (datum.tabasco !== undefined) {
+                    return yScale(datum.tabasco)
+                } else if (datum.sriracha !== undefined) {
+                    return yScale(datum.sriracha)
+                }
             })
-            .attr("cy", datum => yScale(datum.sriracha))
             .attr("r", "2")
-            .attr("fill", "orange");
+            .attr("fill", datum => {
+                if (datum.tabasco !== undefined) {
+                    return "green";
+                } else if (datum.sriracha !== undefined) {
+                    return "orange";
+                }
+            });
     });
 }
-
-
-// // plot data as circles
-// let circ = svg.selectAll('circle')  // selects all circles (even though they haven't been create yet!)
-//     .data(dataset)  // bind data to DOM (bind each circle to a data point)
-//     .enter()        // add one circle per data point
-//     .append('circle')
-//     .attr('cx', function (d) { return xScale(d[0]); })   // attributes for each circle are a function of the data points (scaled to the range)
-//     .attr('cy', function (d) { return yScale(d[1]); })
-//     .attr('r', function (d) { return rScale(d[2]); })
-//     .attr('fill', 'purple').attr('opacity', 0.5);
-
-
-// // add axes to svg
-// // NOTE about translate: translate(origin-x-coord, origin-y-coord) ... remember y origin starts at top
-
-// // x axis
-// svg.append('g') // creates a group
-//     .attr('class', 'axis')  // add css class for styling
-    // .attr("transform", "translate(0," + (h - pad) + ")") // translate(origin-x-coord, origin-y-coord)
-//     .call(xAxis);
-// // y axis
-// svg.append('g')
-//     .attr('class', 'axis')
-//     .attr("transform", "translate(" + pad + ", 0)")
-//     .call(yAxis);
